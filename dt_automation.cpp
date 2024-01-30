b4b2ce77eef27eee70c290622dfee759a383ed5a
aaeu-mirza <mirzatauheed.baig@alpsalpine.com>
2024-01-30 09:59:22 +0100

    [Update]: added printing of value + minor fixes

--- dt_automation.cpp
+++ dt_automation.cpp
@@ -30,16 +30,68 @@ while(0)
 
 int counter = 0;
 
+void 
+process_data(HDASS hAD, HBUF hBuffer)
+{
+   DBL min=0,max=0;
+   DBL volts;
+   ULNG value;
+   ULNG samples;
+   UINT encoding=0,resolution=0;
+   PDWORD  pBuffer32 = NULL;
+   PWORD  pBuffer = NULL;
+
+   if( hBuffer )
+   {
+
+      /* get sub system information for code/volts conversion */
+      olDaGetRange(hAD,&max,&min);
+      olDaGetEncoding(hAD,&encoding);
+      olDaGetResolution(hAD,&resolution);
+
+      /* get max samples in input buffer */
+      olDmGetValidSamples( hBuffer, &samples );
+
+      /* get pointer to the buffer */
+      if (resolution > 16)
+      {
+         olDmGetBufferPtr( hBuffer,(LPVOID*)&pBuffer32);
+         /* get last sample in buffer */
+         value = pBuffer32[samples-1];
+      }
+      else
+      {
+         olDmGetBufferPtr( hBuffer,(LPVOID*)&pBuffer);
+         /* get last sample in buffer */
+         value = pBuffer[samples-1];
+      }
+      /*  convert value to volts */
+
+      if (encoding != OL_ENC_BINARY) 
+      {
+         /* convert to offset binary by inverting the sign bit */
+
+         value ^= 1L << (resolution-1);
+         value &= (1L << resolution) - 1;     /* zero upper bits */
+      }
+
+      volts = ((float)max-(float)min)/(1L<<resolution)*value + (float)min;
+      printf("%lf V - ", volts);
+   }
+}
+
+
 LRESULT WINAPI 
 WndProc( HWND hWnd, UINT msg, WPARAM hAD, LPARAM lParam )
 {
     switch( msg )
     {
        case OLDA_WM_BUFFER_DONE:
-          printf( "Buffer Done Count : %ld \r", counter );
+          printf( "Buffer Done Count: %ld \r", counter );
           HBUF hBuf;
           counter++;
           olDaGetBuffer( (HDASS)hAD, &hBuf );
+          process_data((HDASS)hAD, hBuf);
           olDaPutBuffer( (HDASS)hAD, hBuf );
           break;
 
@@ -88,105 +140,105 @@ EnumBrdProc( LPSTR lpszBrdName, LPSTR lpszDriverName, LPARAM lParam )
    return FALSE;    // all set , board handle in lParam
 }
 
-
-void 
-main()
+int main()
 {
-    printf( "Open Layers Continuous A/D Win32 Console Example\n" );
+   int i = 0;
+   printf( "Open Layers Continuous A/D Win32 Console Example\n" );
 
-    // create a window for messages
-    WNDCLASS wc;
-    memset( &wc, 0, sizeof(wc) );
-    wc.lpfnWndProc = WndProc;
-    wc.lpszClassName = "DtConsoleClass";
-    RegisterClass( &wc );
+   // create a window for messages
+   WNDCLASS wc;
+   memset( &wc, 0, sizeof(wc) );
+   wc.lpfnWndProc = WndProc;
+   wc.lpszClassName = "DtConsoleClass";
+   RegisterClass( &wc );
 
-    HWND hWnd = CreateWindow( wc.lpszClassName,
-                              NULL, 
-                              NULL, 
-                              0, 0, 0, 0,
-                              NULL,
-                              NULL,
-                              NULL,
-                              NULL );
+   HWND hWnd = CreateWindow( wc.lpszClassName,
+                           NULL, 
+                           NULL, 
+                           0, 0, 0, 0,
+                           NULL,
+                           NULL,
+                           NULL,
+                           NULL );
 
-    if( !hWnd )
-       exit( 1 );
+   if( !hWnd )
+      exit( 1 );
 
-    HDEV hDev = NULL;
-    CHECKERROR( olDaEnumBoards( EnumBrdProc, (LPARAM)&hDev ) );
+   HDEV hDev = NULL;
+   CHECKERROR( olDaEnumBoards( EnumBrdProc, (LPARAM)&hDev ) );
 
-    HDASS hAD = NULL;
-    CHECKERROR( olDaGetDASS( hDev, OLSS_AD, 0, &hAD ) ); 
+   HDASS hAD = NULL;
+   CHECKERROR( olDaGetDASS( hDev, OLSS_AD, 0, &hAD ) ); 
 
-    CHECKERROR( olDaSetWndHandle( hAD, hWnd, 0 ) ); 
+   CHECKERROR( olDaSetWndHandle( hAD, hWnd, 0 ) ); 
 
-    CHECKERROR( olDaSetDataFlow( hAD, OL_DF_CONTINUOUS ) ); 
-    CHECKERROR( olDaSetChannelListSize( hAD, 1 ) );
-    CHECKERROR( olDaSetChannelListEntry( hAD, 0, 0 ) );
-    CHECKERROR( olDaSetGainListEntry( hAD, 0, 1 ) );
-    CHECKERROR( olDaSetTrigger( hAD, OL_TRG_SOFT ) );
-    CHECKERROR( olDaSetClockSource( hAD, OL_CLK_INTERNAL ) ); 
-    CHECKERROR( olDaSetClockFrequency( hAD, 1000.0 ) );
-    CHECKERROR( olDaSetWrapMode( hAD, OL_WRP_NONE ) );
-    CHECKERROR( olDaConfig( hAD ) );
+   CHECKERROR( olDaSetDataFlow( hAD, OL_DF_CONTINUOUS ) ); 
+   CHECKERROR( olDaSetChannelListSize( hAD, 1 ) );
+   CHECKERROR( olDaSetChannelListEntry( hAD, 0, 0 ) );
+   CHECKERROR( olDaSetGainListEntry( hAD, 0, 1 ) );
+   CHECKERROR( olDaSetTrigger( hAD, OL_TRG_SOFT ) );
+   CHECKERROR( olDaSetClockSource( hAD, OL_CLK_INTERNAL ) ); 
+   CHECKERROR( olDaSetClockFrequency( hAD, 1000.0 ) );
+   CHECKERROR( olDaSetWrapMode( hAD, OL_WRP_NONE ) );
+   CHECKERROR( olDaConfig( hAD ) );
 
-    HBUF hBufs[NUM_OL_BUFFERS];
-    for( int i=0; i < NUM_OL_BUFFERS; i++ )
-    {
-        if( OLSUCCESS != olDmAllocBuffer( GHND, 1000, &hBufs[i] ) )
-        {
-           for ( i--; i>=0; i-- )
-           {
-              olDmFreeBuffer( hBufs[i] );
-           }   
-           exit( 1 );   
-        }
-        olDaPutBuffer( hAD,hBufs[i] );
-    }
+   HBUF hBufs[NUM_OL_BUFFERS];
+   for( int i=0; i < NUM_OL_BUFFERS; i++ )
+   {
+      if( OLSUCCESS != olDmAllocBuffer( GHND, 1000, &hBufs[i] ) )
+      {
+         for ( i--; i>=0; i-- )
+         {
+            olDmFreeBuffer( hBufs[i] );
+         }   
+         exit( 1 );   
+      }
+      olDaPutBuffer( hAD,hBufs[i] );
+   }
 
-    if( OLSUCCESS != ( olDaStart( hAD ) ) )
-    {
-       printf( "A/D Operation Start Failed...hit any key to terminate.\n" );
-    }
-    else
-    {
-       printf( "A/D Operation Started...hit any key to terminate.\n\n" );
-       printf( "Buffer Done Count : %ld \r", counter );
-    }   
+   if( OLSUCCESS != ( olDaStart( hAD ) ) )
+   {
+      printf( "A/D Operation Start Failed...hit any key to terminate.\n" );
+   }
+   else
+   {
+      printf( "A/D Operation Started...hit any key to terminate.\n\n" );
+      printf( "Buffer Done Count : %ld \r", counter );
+   }   
 
-    MSG msg;                                    
-    SetMessageQueue( 50 );                      // Increase the our message queue size so
-                                                // we don't lose any data acq messages
- 
-    // Acquire and dispatch messages until a key is hit...since we are a console app 
-    // we are using a mix of Windows messages for data acquistion and console approaches
-    // for keyboard input.
-    //
-    while( GetMessage( &msg,        // message structure              
-                       hWnd,        // handle of window receiving the message 
-                       0,           // lowest message to examine          
-                       0 ) )        // highest message to examine         
-    {
-       TranslateMessage( &msg );    // Translates virtual key codes       
-       DispatchMessage( &msg );     // Dispatches message to window 
-       if( _kbhit() )
-       {
-          _getch();
-          PostQuitMessage(0);      
-       }
-    }
+   MSG msg;                                    
+   SetMessageQueue( 50 );                      // Increase the our message queue size so
+                                             // we don't lose any data acq messages
 
-    // abort A/D operation
-    olDaAbort( hAD );
-    printf( "\nA/D Operation Terminated \n" );
+   // Acquire and dispatch messages until a key is hit...since we are a console app 
+   // we are using a mix of Windows messages for data acquistion and console approaches
+   // for keyboard input.
+   //
+   while( GetMessage( &msg,        // message structure              
+                     hWnd,        // handle of window receiving the message 
+                     0,           // lowest message to examine          
+                     0 ) )        // highest message to examine         
+   {
+      TranslateMessage( &msg );    // Translates virtual key codes       
+      DispatchMessage( &msg );     // Dispatches message to window 
+      if( _kbhit() )
+      {
+         _getch();
+         PostQuitMessage(0);      
+      }
+   }
 
-    for( i=0; i<NUM_OL_BUFFERS; i++ ) 
-    {
-       olDmFreeBuffer( hBufs[i] );
-    }   
+   // abort A/D operation
+   olDaAbort( hAD );
+   printf( "\nA/D Operation Terminated \n" );
 
-    olDaTerminate( hDev );
-    exit( 0 );
+   for( i=0; i<NUM_OL_BUFFERS; i++ ) 
+   {
+      olDmFreeBuffer( hBufs[i] );
+   }   
 
+   olDaTerminate( hDev );
+   exit( 0 );
+
+   return 0;
 }
