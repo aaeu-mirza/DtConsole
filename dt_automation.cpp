98b1226f9bd8b8ad43713659f9d8206cfff4344f
aaeu-mirza <mirzatauheed.baig@alpsalpine.com>
2024-02-13 14:31:49 +0100

    [Added]: channel 4 input recording + output square waveform generation

--- dt_automation.cpp
+++ dt_automation.cpp
@@ -11,25 +11,32 @@ PURPOSE:
 
 #include <windows.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <conio.h>
+#include <math.h>
 #include "oldaapi.h" // requires Open Layers Data Aquisition (olDa) packaged lib files.
 
 /* Config Params*/
 #define NUM_CHANNELS 4 // Max 4 for DT9837
-#define ALL_CHANNEL_GAIN 100
-#define CLOCK_FREQUENCY 1000.0
+#define ALL_CHANNEL_GAIN 1 // Universal gain used for all channels (1 or 10)
+#define CLOCK_FREQUENCY 1000.0 // Internal clock frequency
+#define OUTPUT_FREQUENCY 46876.0 // Internal clock frequency
+#define DEFAULT_WAV_AMPLITUDE 3 //for output
+#define DEFAULT_WAV_FREQUENCY 100 //for output
+
 #define EN_MULTIPLE_CH_GAIN 1 //(1-ON,0-OFF)
 
 #if EN_MULTIPLE_CH_GAIN == 1
-#define CHANNEL_GAIN_0 100 // Z
-#define CHANNEL_GAIN_1 100 // Y
-#define CHANNEL_GAIN_2 100 // X
-#define CHANNEL_GAIN_3 100 // N/A
+#define CHANNEL_GAIN_0 1 // Z
+#define CHANNEL_GAIN_1 1 // Y
+#define CHANNEL_GAIN_2 1 // X
+#define CHANNEL_GAIN_3 1 // N/A
 #endif
 
 #define SENSITIVITY_VAL_X 101.3 // mV per g [CALIBRATED: DO NOT CHANGE]
 #define SENSITIVITY_VAL_Y 99.7  // mV per g [CALIBRATED: DO NOT CHANGE]
 #define SENSITIVITY_VAL_Z 101.9 // mV per g [CALIBRATED: DO NOT CHANGE]
+
 /* olDa error checking */
 #define CHECKERROR(ecode)                           \
    do                                               \
@@ -42,7 +49,12 @@ PURPOSE:
       }                                             \
    } while (0)
 
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
 #define NUM_OL_BUFFERS 4
+#define MAX_BUFFER_SIZE 8000 // 8192 rounded to 8k for even output repeatability
+#define AVERAGING_CONSTANT 10 // observed from buffer to output conversion
 
 int counter = 0;
 BOOL tfileopen = 0;
@@ -116,7 +128,7 @@ BOOL save_data(HDASS hAD, HBUF hBuf)
       stream = fopen("accel.csv", "w"); // open new file
       tfileopen = 1;
       textfile_time = 0;
-      fprintf(stream, "Time,accel(x),accel(y),accel(z)\n");
+      fprintf(stream, "Time,accel(x),accel(y),accel(z),dac\n");
    }
    /* get pointer to the buffer */
    CHECKERROR(olDmGetBufferPtr(hBuf, (LPVOID *)&pBuffer32));
@@ -126,8 +138,8 @@ BOOL save_data(HDASS hAD, HBUF hBuf)
       if (resolution > 16)
       {
          // Value reserved for channel 4
-         // value = pBuffer32[i+3];
-         // olDaCodeToVolts(min, max, gainlist[j] /*gain*/, resolution, encoding, value, &voltage);
+         value = pBuffer32[i+3];
+         olDaCodeToVolts(min, max, gainlist[j] /*gain*/, resolution, encoding, value, &voltage);
 
          // Convert and store values
          value = pBuffer32[i + 2];
@@ -140,8 +152,8 @@ BOOL save_data(HDASS hAD, HBUF hBuf)
       else
       {
          // Value reserved for channel 4
-         // value = pBuffer[i+3];
-         // olDaCodeToVolts(min, max, gainlist[j] /*gain*/, resolution, encoding, value, &voltage);
+         value = pBuffer[i+3];
+         olDaCodeToVolts(min, max, gainlist[j] /*gain*/, resolution, encoding, value, &voltage);
 
          // Convert and store values
          value = pBuffer[i + 2];
@@ -159,7 +171,7 @@ BOOL save_data(HDASS hAD, HBUF hBuf)
       accel_z = volt_z / (SENSITIVITY_VAL_Z / 1000);
 
       // Print voltage values to file
-      fprintf(stream, "%.3f,%f,%f,%f\n", textfile_time, accel_x, accel_y, accel_z);
+      fprintf(stream, "%.3f,%f,%f,%f,%f\n", textfile_time, accel_x, accel_y, accel_z, voltage);
 
       textfile_time += (1 / freq);
       // i++;
@@ -295,91 +307,286 @@ EnumBrdProc(LPSTR lpszBrdName, LPSTR lpszDriverName, LPARAM lParam)
    printf("%s succesfully initialized.\n", lpszBrdName);
    return FALSE; // all set , board handle in lParam
 }
-extern "C"
+
+void measure(bool use_default_values, int num_channels, float clk_freq, int all_channel_gain, int channel_0_gain, int channel_1_gain, int channel_2_gain, int channel_3_gain)
 {
-   void measure(bool use_default_values, int num_channels, float clk_freq, int all_channel_gain, int channel_0_gain, int channel_1_gain, int channel_2_gain, int channel_3_gain)
+   printf("def: %d %d %d\n", use_default_values, clk_freq, all_channel_gain);
+   if (use_default_values)
    {
-      if (use_default_values)
+      num_channels = NUM_CHANNELS;
+      all_channel_gain = ALL_CHANNEL_GAIN;
+      channel_0_gain = CHANNEL_GAIN_0;
+      channel_1_gain = CHANNEL_GAIN_1;
+      channel_2_gain = CHANNEL_GAIN_2;
+      channel_3_gain = CHANNEL_GAIN_3;
+      clk_freq = CLOCK_FREQUENCY;
+   }
+
+   int i = 0;
+   printf("Open Layers Continuous A/D Win32 Console Example\n");
+
+   // create a window for messages
+   WNDCLASS wc;
+   memset(&wc, 0, sizeof(wc));
+   wc.lpfnWndProc = WndProc;
+   wc.lpszClassName = "DtConsoleClass";
+   RegisterClass(&wc);
+
+   HWND hWnd = CreateWindow(wc.lpszClassName,
+                              NULL,
+                              NULL,
+                              0, 0, 0, 0,
+                              NULL,
+                              NULL,
+                              NULL,
+                              NULL);
+
+   if (!hWnd)
+      exit(1);
+
+   /* Configuring the board*/
+   HDEV hDev = NULL;
+   HDASS hAD = NULL;
+   COUPLING_TYPE coup;
+
+   CHECKERROR(olDaEnumBoards(EnumBrdProc, (LPARAM)&hDev));
+   CHECKERROR(olDaGetDASS(hDev, OLSS_AD, 0, &hAD));
+   CHECKERROR(olDaSetWndHandle(hAD, hWnd, 0));
+   CHECKERROR(olDaSetDataFlow(hAD, OL_DF_CONTINUOUS));
+
+   CHECKERROR(olDaSetChannelListSize(hAD, num_channels));
+   for (int i = 0; i < num_channels; i++)
+   {
+      /* Set Channel List and index */
+      CHECKERROR(olDaSetChannelListEntry(hAD, i, i));
+
+      /* Set Channel Gain Values */
+      CHECKERROR(olDaSetGainListEntry(hAD, i, all_channel_gain));
+
+      /* Set channels coupling type to AC coupling */
+      CHECKERROR(olDaSetCouplingType(hAD, i, AC));
+
+      /* Set channels current source to disabled */
+      CHECKERROR(olDaSetExcitationCurrentSource(hAD, i, INTERNAL));
+   }
+
+#if EN_MULTIPLE_CH_GAIN == 1
+   /* Set individual Channel Gain Values */
+   CHECKERROR(olDaSetGainListEntry(hAD, 0, channel_0_gain));
+   CHECKERROR(olDaSetGainListEntry(hAD, 1, channel_1_gain));
+   CHECKERROR(olDaSetGainListEntry(hAD, 2, channel_2_gain));
+   CHECKERROR(olDaSetGainListEntry(hAD, 3, channel_3_gain));
+#endif
+   /* Set the clock and frequency for data acquisition*/
+   CHECKERROR(olDaSetTrigger(hAD, OL_TRG_SOFT));
+   CHECKERROR(olDaSetClockSource(hAD, OL_CLK_INTERNAL));
+   CHECKERROR(olDaSetClockFrequency(hAD, clk_freq));
+   CHECKERROR(olDaSetWrapMode(hAD, OL_WRP_NONE));
+   // CHECKERROR(olDaSetWrapMode(hAD, OL_WRP_MULTIPLE));
+
+   /* Store the config*/
+   CHECKERROR(olDaConfig(hAD));
+
+   /* Allocating memory for data buffers*/
+   HBUF hBufs[NUM_OL_BUFFERS];
+   for (int i = 0; i < NUM_OL_BUFFERS; i++)
+   {
+      // if (OLSUCCESS != olDmAllocBuffer(GHND, (int)clk_freq, &hBufs[i]))
+      if (OLSUCCESS != olDmCallocBuffer(GHND, 0, (int)clk_freq, 2, &hBufs[i]))
       {
-         num_channels = NUM_CHANNELS;
-         all_channel_gain = ALL_CHANNEL_GAIN;
-         channel_0_gain = CHANNEL_GAIN_0;
-         channel_1_gain = CHANNEL_GAIN_1;
-         channel_2_gain = CHANNEL_GAIN_2;
-         channel_3_gain = CHANNEL_GAIN_3;
-         clk_freq = CLOCK_FREQUENCY;
-      }
-
-      int i = 0;
-      printf("Open Layers Continuous A/D Win32 Console Example\n");
-
-      // create a window for messages
-      WNDCLASS wc;
-      memset(&wc, 0, sizeof(wc));
-      wc.lpfnWndProc = WndProc;
-      wc.lpszClassName = "DtConsoleClass";
-      RegisterClass(&wc);
-
-      HWND hWnd = CreateWindow(wc.lpszClassName,
-                               NULL,
-                               NULL,
-                               0, 0, 0, 0,
-                               NULL,
-                               NULL,
-                               NULL,
-                               NULL);
-
-      if (!hWnd)
+         for (i--; i >= 0; i--)
+         {
+            olDmFreeBuffer(hBufs[i]);
+         }
          exit(1);
+      }
+      olDaPutBuffer(hAD, hBufs[i]);
+   }
 
-      /* Configuring the board*/
-      HDEV hDev = NULL;
-      HDASS hAD = NULL;
-      COUPLING_TYPE coup;
+   /* Start acquisition*/
+   if (OLSUCCESS != (olDaStart(hAD)))
+   {
+      printf("A/D Operation Start Failed...hit any key to terminate.\n");
+   }
+   else
+   {
+      printf("A/D Operation Started...hit any key to terminate.\n\n");
+      printf("Buffer Done Count : %ld \r", counter);
+   }
 
-      CHECKERROR(olDaEnumBoards(EnumBrdProc, (LPARAM)&hDev));
+   MSG msg;
+   SetMessageQueue(50); // Increase the our message queue size so
+                        // we don't lose any data acq messages
+
+   // Acquire and dispatch messages until a key is hit...since we are a console app
+   // we are using a mix of Windows messages for data acquistion and console approaches
+   // for keyboard input.
+   //
+   while (GetMessage(&msg, // message structure
+                     hWnd, // handle of window receiving the message
+                     0,    // lowest message to examine
+                     0))   // highest message to examine
+   {
+      TranslateMessage(&msg); // Translates virtual key codes
+      DispatchMessage(&msg);  // Dispatches message to window
+      if (_kbhit())
+      {
+         _getch();
+         PostQuitMessage(0);
+      }
+   }
+
+   // abort A/D operation
+   olDaAbort(hAD);
+   printf("\nA/D Operation Terminated \n");
+
+   for (i = 0; i < NUM_OL_BUFFERS; i++)
+   {
+      olDmFreeBuffer(hBufs[i]);
+   }
+
+   olDaTerminate(hDev);
+   exit(0);
+}
+/* This function generates a simple squarewave at the specified amplitude, frequency and duration (in s) */
+void generate(bool use_default_values, bool read_input, float clk_freq, int all_channel_gain, int amplitude, int wave_freq, int duration)
+{
+   if (use_default_values)
+   {
+      all_channel_gain = ALL_CHANNEL_GAIN;
+      clk_freq = OUTPUT_FREQUENCY;
+      wave_freq = DEFAULT_WAV_FREQUENCY;
+      amplitude = DEFAULT_WAV_AMPLITUDE;
+   }
+
+   DBL min,max,freq;
+   float volts;
+   long minvalue,maxvalue;
+   UINT encoding,resolution;
+   UINT size,peak,dma,i,j;
+   UINT wavefreq;
+   UINT channel = 0;
+
+   printf("Open Layers Continuous D/A Win32 Console Example\n");
+
+   // create a window for messages
+   WNDCLASS wc;
+   memset(&wc, 0, sizeof(wc));
+   wc.lpfnWndProc = WndProc;
+   wc.lpszClassName = "DtConsoleClass";
+   RegisterClass(&wc);
+
+   HWND hWnd = CreateWindow(wc.lpszClassName,
+                              NULL,
+                              NULL,
+                              0, 0, 0, 0,
+                              NULL,
+                              NULL,
+                              NULL,
+                              NULL);
+
+   if (!hWnd)
+      exit(1);
+
+   /* Configuring the board*/
+   HDEV hDev = NULL;
+   HDASS hDA = NULL;
+   HDASS hAD = NULL;
+   HBUF hBuf = NULL;
+   HBUF hBufs[NUM_OL_BUFFERS];
+   PWORD lpbuf= NULL;    
+
+   CHECKERROR(olDaEnumBoards(EnumBrdProc, (LPARAM)&hDev));
+
+   CHECKERROR(olDaGetDASS(hDev, OLSS_DA, 0, &hDA));
+   CHECKERROR (olDaGetSSCapsEx(hDA,OLSSCE_MAXTHROUGHPUT,&freq));
+   CHECKERROR (olDaGetSSCaps(hDA,OLSSC_NUMDMACHANS,&dma));
+
+   dma  = MIN(1, dma);            /* try for one dma channel   */ 
+   freq = clk_freq;
+
+   CHECKERROR(olDaSetWndHandle(hDA, hWnd, (UINT)NULL));
+   CHECKERROR(olDaSetDataFlow(hDA, OL_DF_CONTINUOUS));
+
+   CHECKERROR(olDaSetChannelListSize(hDA, 1));
+   CHECKERROR(olDaSetChannelListEntry(hDA, 0, 0));
+   CHECKERROR(olDaSetGainListEntry(hDA, 0, all_channel_gain));
+
+   /* Set the clock and frequency for data acquisition*/
+   CHECKERROR(olDaSetClockFrequency(hDA, freq));
+   CHECKERROR(olDaSetDmaUsage(hDA,dma));
+   CHECKERROR(olDaSetWrapMode(hDA, OL_WRP_SINGLE));
+
+   /* Store the config*/
+   CHECKERROR(olDaConfig(hDA));
+
+   /* get sub system information for code/volts conversion */
+   CHECKERROR (olDaGetRange(hDA,&max,&min));
+   CHECKERROR (olDaGetEncoding(hDA,&encoding));
+   CHECKERROR (olDaGetResolution(hDA,&resolution));
+
+   if(read_input)
+   {
       CHECKERROR(olDaGetDASS(hDev, OLSS_AD, 0, &hAD));
       CHECKERROR(olDaSetWndHandle(hAD, hWnd, 0));
       CHECKERROR(olDaSetDataFlow(hAD, OL_DF_CONTINUOUS));
-
-      CHECKERROR(olDaSetChannelListSize(hAD, num_channels));
-      for (int i = 0; i < num_channels; i++)
-      {
-         /* Set Channel List and index */
-         CHECKERROR(olDaSetChannelListEntry(hAD, i, i));
-
-         /* Set Channel Gain Values */
-         CHECKERROR(olDaSetGainListEntry(hAD, i, all_channel_gain));
-
-         /* Set channels coupling type to AC coupling */
-         CHECKERROR(olDaSetCouplingType(hAD, i, AC));
-
-         /* Set channels current source to disabled */
-         CHECKERROR(olDaSetExcitationCurrentSource(hAD, i, INTERNAL));
-      }
-
-#if EN_MULTIPLE_CH_GAIN == 1
-      /* Set individual Channel Gain Values */
-      CHECKERROR(olDaSetGainListEntry(hAD, 0, channel_0_gain));
-      CHECKERROR(olDaSetGainListEntry(hAD, 1, channel_1_gain));
-      CHECKERROR(olDaSetGainListEntry(hAD, 2, channel_2_gain));
-      CHECKERROR(olDaSetGainListEntry(hAD, 3, channel_3_gain));
-#endif
-      /* Set the clock and frequency for data acquisition*/
+      CHECKERROR(olDaSetChannelListSize(hAD, 4));
+      CHECKERROR(olDaSetChannelListEntry(hAD, 0, 0));
+      CHECKERROR(olDaSetChannelListEntry(hAD, 1, 1));
+      CHECKERROR(olDaSetChannelListEntry(hAD, 2, 2));
+      CHECKERROR(olDaSetChannelListEntry(hAD, 3, 3));
+      CHECKERROR(olDaSetCouplingType(hAD, 3, AC));
+      CHECKERROR(olDaSetExcitationCurrentSource(hAD, 3, INTERNAL));
+      CHECKERROR(olDaSetClockFrequency(hAD, 1000));
+      CHECKERROR(olDaSetWrapMode(hAD, OL_WRP_NONE));
       CHECKERROR(olDaSetTrigger(hAD, OL_TRG_SOFT));
       CHECKERROR(olDaSetClockSource(hAD, OL_CLK_INTERNAL));
-      CHECKERROR(olDaSetClockFrequency(hAD, clk_freq));
-      CHECKERROR(olDaSetWrapMode(hAD, OL_WRP_NONE));
-      // CHECKERROR(olDaSetWrapMode(hAD, OL_WRP_MULTIPLE));
-
-      /* Store the config*/
       CHECKERROR(olDaConfig(hAD));
+   }
 
+
+   /* convert max full scale to DAC units */
+   volts = amplitude;
+   olDaVoltsToCode(min,max, 1 /*gain*/, resolution, encoding, volts, &maxvalue);
+
+   /* convert min full scale to DAC units */
+   volts = -(amplitude);
+   olDaVoltsToCode(min,max, 1 /*gain*/, resolution, encoding, volts, &minvalue);
+   
+   /* 100 hz square wave or the best the board can do */
+   wavefreq = wave_freq; //10Hz - 400Hz
+   size = MAX_BUFFER_SIZE;
+   peak = (size*AVERAGING_CONSTANT)/wavefreq;
+
+   /* allocate the output buffer */
+   CHECKERROR (olDmCallocBuffer(GMEM_FIXED,0,(ULNG) size,2,&hBuf));
+   CHECKERROR (olDmGetBufferPtr(hBuf,(LPVOID *)&lpbuf));
+
+   double angle = 0;
+   /* fill the output buffer with a simple square wave */
+   for (i=0,j=0;i<size;i+=peak)
+   {
+      while (j<i+(peak/2))
+            lpbuf[j++] = (UINT)minvalue;
+      while (j<i+peak)
+            lpbuf[j++] = (UINT)maxvalue;
+   }
+
+   /* Test Sin Wave Implementation (DOESNT WORK)*/
+   // for (i=0,j=0;i<size;i+=peak)
+   // {
+   //    angle = 2.0 * 3.14159265 * i / size;
+   //    volts = (amplitude * sin(angle));
+   //    olDaVoltsToCode(min,max, 1 /*gain*/, resolution, encoding, volts, &minvalue);
+   //    printf("val:%f %d\n",volts,minvalue);
+   //    lpbuf[i++] = (UINT)minvalue;
+   // }
+
+   if(read_input)
+   {
       /* Allocating memory for data buffers*/
-      HBUF hBufs[NUM_OL_BUFFERS];
       for (int i = 0; i < NUM_OL_BUFFERS; i++)
       {
-         // if (OLSUCCESS != olDmAllocBuffer(GHND, (int)clk_freq, &hBufs[i]))
          if (OLSUCCESS != olDmCallocBuffer(GHND, 0, (int)clk_freq, 2, &hBufs[i]))
          {
             for (i--; i >= 0; i--)
@@ -390,7 +597,27 @@ extern "C"
          }
          olDaPutBuffer(hAD, hBufs[i]);
       }
+   }
 
+   /* for DAC's must set the number of valid samples in buffer */
+   CHECKERROR (olDmSetValidSamples(hBuf,size));
+
+   /* Put the buffer to the DAC */
+   CHECKERROR (olDaPutBuffer(hDA, hBuf));
+
+   /* Start acquisition*/
+   if (OLSUCCESS != (olDaStart(hDA)))
+   {
+      printf("D/A Operation Start Failed...hit any key to terminate.\n");
+   }
+   else
+   {
+      printf("D/A Operation Started...hit any key to terminate.\n\n");
+   }
+   
+   MSG msg;
+   if(read_input)
+   {
       /* Start acquisition*/
       if (OLSUCCESS != (olDaStart(hAD)))
       {
@@ -399,10 +626,8 @@ extern "C"
       else
       {
          printf("A/D Operation Started...hit any key to terminate.\n\n");
-         printf("Buffer Done Count : %ld \r", counter);
       }
 
-      MSG msg;
       SetMessageQueue(50); // Increase the our message queue size so
                            // we don't lose any data acq messages
 
@@ -423,7 +648,15 @@ extern "C"
             PostQuitMessage(0);
          }
       }
+   }
 
+   /*
+      get the output buffer from the DAC subsystem and
+      free the output buffer
+   */
+   olDaAbort(hDA);
+   if(read_input)
+   {
       // abort A/D operation
       olDaAbort(hAD);
       printf("\nA/D Operation Terminated \n");
@@ -432,8 +665,19 @@ extern "C"
       {
          olDmFreeBuffer(hBufs[i]);
       }
-
-      olDaTerminate(hDev);
-      exit(0);
+      CHECKERROR (olDaReleaseDASS(hAD));
    }
+   CHECKERROR (olDaGetBuffer(hDA, &hBuf));
+   CHECKERROR (olDmFreeBuffer(hBuf));
+
+   /* release the subsystem and the board */
+
+   CHECKERROR (olDaReleaseDASS(hDA));
+   CHECKERROR (olDaTerminate(hDev));
+   exit(0);
+}
+
+void hello(char* str, int age)
+{
+   printf("%s %d",str,age);
 }
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import ctypes
 
 # Config Params
 NUM_CHANNELS = 4  # Max 4 for DT9837
-ALL_CHANNEL_GAIN = 1
+ALL_CHANNEL_GAIN = 1  # 1 or 10
 CLOCK_FREQUENCY = 1000.0
 
 CHANNEL_GAIN_0 = 1  # Z
@@ -11,22 +11,40 @@ CHANNEL_GAIN_1 = 1  # Y
 CHANNEL_GAIN_2 = 1  # X
 CHANNEL_GAIN_3 = 1  # N/A
 
+WAVEFORM_AMPLITUDE = 3  # in volts (V)
+WAVEFORM_FREQUENCY = 10  # in hertz (Hz)
+WAVEFORM_DURATION = 10  # in seconds (s)
+
 
 class DT9837():
     def __init__(self):
-        dt_lib = ctypes.CDLL(
+        self.dt_lib = ctypes.CDLL(
             "C:\_Automation\Win32\SDK\Examples\DtConsole\dt_lib.so")
-        self.measure = dt_lib.measure
 
     def measure(self, use_default_vals=True):
+        self.measure = self.dt_lib.measure
         self.measure.argtypes = [ctypes.c_bool, ctypes.c_int, ctypes.c_float,
                                  ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
         self.measure(use_default_vals, NUM_CHANNELS,
                      CLOCK_FREQUENCY, ALL_CHANNEL_GAIN, CHANNEL_GAIN_0, CHANNEL_GAIN_1, CHANNEL_GAIN_2, CHANNEL_GAIN_3)
+
+    def generate(self, use_default_vals=True, read_input=False):
+        self.generate = self.dt_lib.generate
+        self.generate.argtypes = [
+            ctypes.c_bool,  ctypes.c_bool, ctypes.c_float, ctypes.c_int, ctypes.c_int, ctypes.c_int]
+        self.generate(use_default_vals, read_input, 1000.0, ALL_CHANNEL_GAIN,
+                      WAVEFORM_AMPLITUDE, WAVEFORM_FREQUENCY, WAVEFORM_DURATION)
+
+    def hello(self):
+        self.hello = self.dt_lib.hello
+        self.hello.argtypes = [ctypes.c_char_p, ctypes.c_int]
+        self.hello(b"Mirza", 18)
 
 
 if __name__ == "__main__":
     signalanalyzer = DT9837()
 
     # signalanalyzer.measure()
-    signalanalyzer.measure(use_default_vals=False)
+    # signalanalyzer.measure(use_default_vals=False)
+    signalanalyzer.generate(use_default_vals=True, read_input=True)
+    # signalanalyzer.hello()
